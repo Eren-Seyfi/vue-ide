0f804994ae0d67870d67a2be9e12d50fb8c4e594
Eren Seyfi <45491369+Eren-Seyfi@users.noreply.github.com>
2023-12-28 00:06:27 +0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="editor"></div>
+</template>
+
+<script setup>
+import { onMounted } from "vue";
+import ace from "ace-builds";
+
+onMounted(() => {
+
+  const editor = ace.edit('editor', {
+    maxLines: "100%",
+    minLines: 4,
+    fontSize: 14,
+    theme: 'ace/theme/monokai',
+    mode: 'ace/mode/javascript',
+    tabSize: 4,
+    showPrintMargin: false,
+  });
+
+  editor.setOptions({
+    enableBasicAutocompletion: true,
+    enableLiveAutocompletion: true
+  });
+
+  editor.getSession().setValue('function foo(items) { var x = "All this is syntax highlighted"; return x; }');
+
+  editor.getSession().on('change', () => {
+    console.log(editor.getValue());
+  });
+});
+</script>
+
+<style>
+#editor {
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+}
+</style>
